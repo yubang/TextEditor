be948c606879537465bf184f5bce0aa6698325f9
yubang <yubang@yubang-pc.(none)>
2015-06-13 15:48:31 +0800

    增加字体修改

--- ui/FontFrame.java
+++ ui/FontFrame.java
@@ -6,6 +6,7 @@ import java.awt.GridLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JList;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -15,6 +16,7 @@ public class FontFrame extends JFrame{
 	private JTextArea textArea;
 	private JList<String> list1,list2,list3;
 	private DefaultListModel<String> m1,m2,m3;
+	private JScrollPane s1,s2,s3;
 	
 	public FontFrame(JTextArea textArea){
 		
@@ -37,25 +39,36 @@ public class FontFrame extends JFrame{
 		list1=new JList<String>(m1);
 		list2=new JList<String>(m2);
 		list3=new JList<String>(m3);
+		s1=new JScrollPane(list1);
+		s2=new JScrollPane(list2);
+		s3=new JScrollPane(list3);
 		
+		m1.addElement("12");
+		m1.addElement("14");
+		m1.addElement("16");
+		m1.addElement("18");
 		m1.addElement("20");
+		m1.addElement("22");
+		m1.addElement("24");
 		m1.addElement("26");
+		m1.addElement("28");
 		m1.addElement("30");
+		m1.addElement("32");
 		
 		m2.addElement("正常");
 		m2.addElement("粗体");
 		
 		m3.addElement("宋体");
-		m3.addElement("宋体");
-		m3.addElement("宋体");
+		m3.addElement("楷体");
+		m3.addElement("黑体");
 		
 		list1.setSelectedIndex(0);
 		list2.setSelectedIndex(0);
 		list3.setSelectedIndex(0);
 		
-		this.add(list1);
-		this.add(list2);
-		this.add(list3);
+		this.add(s1);
+		this.add(s2);
+		this.add(s3);
 	}
 	
 	public void addListener(){
